4db925368cd0574f8ffe65bf9154b5d55b78ec10
Roger Steve Ruiz <firstworldman@gmail.com>
2013-10-27 02:30:20 -0400

    Add new database and routes for Heroku test
    I'm going to deploy this to Heroku so I'll need a postgresql database
    for the app. I've added the necessary changes to the Gemfile and
    database.yml to use the instructions from the Heroku documentation:
    https://devcenter.heroku.com/articles/getting-started-with-rails4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 VideoPolicySystem::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+  # Get the root based on Country Code
+  get ':country' => 'country#index'
+
+  # Get the root based on Country Code and Video ID
+  get ':country/:id' => 'video#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
