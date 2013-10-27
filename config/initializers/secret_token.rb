c36f0bd45c328eb664fb175dcb223b31a7a3da97
Roger Steve Ruiz <firstworldman@gmail.com>
2013-10-27 00:39:53 -0400

    Add default rails 4 project
    These files were created by the `rails new .` command while being
    inside of the project directory.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoPolicySystem::Application.config.secret_key_base = 'b46742b7e6869c32c379bf43e0ac0ee971ecaff18efa8089754bb50587be1dd9b9a9d953fb235e5fc2734c9dcdbcfb71bb7d368c8ff20f3e61b65c1d980937fc'
