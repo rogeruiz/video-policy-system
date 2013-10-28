fec95da8e7b102b16face4cabd55d628c0684bcd
Roger Steve Ruiz <firstworldman@gmail.com>
2013-10-27 23:55:47 -0400

    Update homepage view with Video placeholders

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @videos = Video.all
+    @systems = System.all
+    
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,3 @@
 class Video < ActiveRecord::Base
+  has_many :policies
 end
