21ce667a47c6e99d71e8d33450a32fa68219d7ac
Roger Steve Ruiz <firstworldman@gmail.com>
2013-10-29 01:27:34 -0400

    Add video view and controller

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,2 +1,13 @@
 class VideoController < ApplicationController
+  def index
+    code = params[:id]
+    selected_video = Video.find_by(id: code)
+    if selected_video
+      @country = Country.find_by(code: params[:code].upcase)
+      @video = selected_video
+    else
+      redirect_to :root
+    end
+
+  end
 end
