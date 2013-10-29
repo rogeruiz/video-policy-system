0e8b0486135e70505621d5059d0e36ed8168b5ab
Roger Steve Ruiz <firstworldman@gmail.com>
2013-10-29 01:27:18 -0400

    Add country view and controller

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -1,4 +1,30 @@
 class CountryController < ApplicationController
   def index
+    code = params[:code].upcase
+    selected_country = Country.find_by(code: code)
+    if selected_country
+      @country = selected_country
+      videos = Video.all
+      data = {'id' => '', 'title' => '', 'country' => '', 'policy' => ''}
+      @videos = []
+      videos.each do |v|
+        v.systems.each do |sys|
+          if sys.country.code == code && sys.policy.name != 'block'
+            data['id'] = v.id
+            data['title'] = v.title
+            data['country'] = sys.country.code
+            data['policy'] = sys.policy.name
+            @videos.push(data.clone)
+            # @policy.push(sys.policy.name)
+            break
+          else
+            next
+          end
+        end
+      end
+    else
+      redirect_to :root
+    end
+
   end
 end
